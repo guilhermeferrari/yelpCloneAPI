939e8198aa1714d5c2d8b6312ee069e29836f68c
guilhermeferrari <guilhermefbravin@gmail.com>
2018-10-09 23:02:09 -0300

    add arquivo json com as informaçoes de restaurantes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Main } from './components/screens'
 import { Map } from './components/containers'
 import { createBottomTabNavigator } from 'react-navigation'
-//pausado em pt4
+import axios from 'axios'
+//pausado em pt6
 const Tabs = createBottomTabNavigator({
   Nearby: Main,
   Search: Map,
@@ -15,11 +16,22 @@ const Tabs = createBottomTabNavigator({
   }
 )
 
+
+
 const db_name = "guilherme"
 const db_pass = "yelproot1"
 const url_db = "mongodb://" + db_name + ":" + db_pass + "@ds125073.mlab.com:25073/yelp_clone_db"
 
 export default class App extends Component {
+
+  
+  componentWillMount(){
+    /* axios.get('https://raw.githubusercontent.com/mongodb/docs-assets/geospatial/restaurants.json')
+      .then( (response) => {
+        console.log(response.data.split("\n"));
+      }) */
+  }
+
   render() {
     return (
       <Tabs />
